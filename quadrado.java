9046d1fe7fcfd69cab88edbab66264e20372a75e
Gabriel Fernandes <gabriel.assuncao.fernandes07@aluno.ifce.edu.br>
2023-09-15 13:50:16 -0300

    fatorial

--- quadrado.java
+++ quadrado.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 class Quadrado {
     public static void main(String[] args) {
-        System.out.println("Aqui é um quadrado");
+        
+        System.out.println("Digite um número");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int numeroInput = scanner.nextInt();
+        int numeroInputQuadrado = numeroInput * numeroInput;
+
+        System.out.print("O quadrado desse número é: " + numeroInputQuadrado);
+
+        scanner.close();
     }
 }
