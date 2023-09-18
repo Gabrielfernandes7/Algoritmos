bb46da2737d6f7787508d2192c0c136b96e197b4
Gabriel Fernandes <gabriel.assuncao.fernandes07@aluno.ifce.edu.br>
2023-09-18 08:43:32 -0300

    1018

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-## Algoritmos - Problemas Beecrowd
-<a href="https://skillicons.dev">
-    <img src="https://skillicons.dev/icons?i=java" />
-</a>
+# Algoritmos - Problemas Beecrowd
 
 - Exercício para fazer: [Tarefa 1006](/tarefa1016.java)
-- Exercício para fazer: [Tarefa 1008](/tarefa1018.java)
--- tarefa1018.java
+++ tarefa1018.java
@@ -1,5 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class tarefa1018 {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        int[] cedulas = { 100, 50, 20, 10, 5, 2, 1 };
         
+        System.out.print("Entre com o capital recebido: ");
+        int capitalRecebido = scanner.nextInt();
+        
+        System.out.println("O valor recebido foi: " + capitalRecebido);
+        System.out.println();
+        
+        for (int i = 0; i < cedulas.length; i++) {            
+            int quantidadeNotas = capitalRecebido / cedulas[i];
+            System.out.printf("%d nota(s) de R$ %d,00\n", quantidadeNotas, cedulas[i]);
+            capitalRecebido = capitalRecebido % cedulas[i]; // O que sobrou para ser trocado
+        }
+        
+        scanner.close();
     }
 }
