a1e937374582175756d732d30e5bb65163888c49
Gabriel Fernandes <gabriel.assuncao.fernandes07@aluno.ifce.edu.br>
2023-10-15 23:35:49 -0300

    resolvendo 1037

--- tarefa1037.java
+++ tarefa1037.java
@@ -1,5 +1,16 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class tarefa1037 {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        Locale.setDefault(Locale.UK);
+
+        System.out.println("Entre com o valor para encontrar em que intervalo ele se encontra");
+        double numero = input.nextDouble();
+
+        if (numero < 0 && numero >= 25) {
+          System.out.println("O valor está fora do intervalo");
+        }
     }
 }
