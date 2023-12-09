2284a38dcf824bf2247e6dcee0feee281ed2c1e0
Gabriel Fernandes <gabriel.assuncao.fernandes07@aluno.ifce.edu.br>
2023-12-09 20:23:38 -0300

    feat: 1041

--- tarefa1041.java
+++ tarefa1041.java
@@ -7,7 +7,27 @@ public class tarefa1041 {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Entre com as coordenadas X, Y");
-        System.out.println("Por meio dessas informações descobriremos o quadrante em que se encontra o ponto");
+        System.out.println("O quadrante do ponto será informado");
+
+        System.out.println("Eixo X: ");
+        double eixoX = input.nextDouble();
+
+        System.out.println("Eixo Y");
+        double eixoY = input.nextDouble();
+
+        if (eixoX == 0 && eixoY == 0) {
+            System.out.println("Origem");
+        } else {
+            if (eixoX > 0 && eixoY > 0) {
+                System.out.println("Q1");
+            } else if (eixoX < 0 && eixoY > 0) {
+                System.out.println("Q2");
+            } else if(eixoX < 0 && eixoY < 0) {
+                System.out.println("Q3");
+            } else if(eixoX > 0 && eixoY < 0) {
+                System.out.println("Q4");
+            }
+        }
 
         input.close();
     }
