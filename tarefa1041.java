bf073c2d3a6ec69af733563b1debfe5907ac78c6
Gabriel Fernandes <gabriel.assuncao.fernandes07@aluno.ifce.edu.br>
2023-12-09 17:05:50 -0300

    feat: solving 1041

---
+++ tarefa1041.java
@@ -0,0 +1,14 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class tarefa1041 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.UK);
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Entre com as coordenadas X, Y");
+        System.out.println("Por meio dessas informações descobriremos o quadrante em que se encontra o ponto");
+
+        input.close();
+    }
+}
