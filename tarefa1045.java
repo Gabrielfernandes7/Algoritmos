bb62aa4d822077045bd286453bea80705f8c00ed
Gabriel Fernandes <gabriel.assuncao.fernandes07@aluno.ifce.edu.br>
2024-01-14 22:41:59 -0300

    1045

--- tarefa1045.java
+++ tarefa1045.java
@@ -1,28 +1,36 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class tarefa1045 {
 
     private static void tipoTriangulo(double a, double b, double c) {
-        if ((a == b) && (a == c) && (b == c)) {
-            System.out.println("Trinângulo equilátero");
+        if ((a >= b + c) || ((a == 0) || (b == 0) || (c == 0))) {
+            System.out.println("Não forma triângulo");
+        } else if((Math.pow(a, 2)) == (Math.pow(b, 2)) + (Math.pow(c, 2))) {
+            System.out.println("Triângulo retângulo");
+        } else if((Math.pow(a, 2)) > (Math.pow(b, 2)) + (Math.pow(c, 2))) {
+            System.out.println("Triângulo obtusângulo");
+        } else if ((Math.pow(a, 2)) < (Math.pow(b, 2)) + (Math.pow(c, 2))) {
+            System.out.println("Triângulo acutângulo");
+        } else if (a == b && a == c){
+            System.out.println("Triângulo equilátero");
         }
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Entre com o lado A");
-        double ladoA = input.nextDouble();
+        System.out.println("Entre com os lados A, B, C");
 
-        System.out.println("Entre com o lado B");
-        double ladoB = input.nextDouble();
-
-        System.out.println("Entre com o lado C");
-        double ladoC = input.nextDouble();
-
-        if (ladoA >= ladoB + ladoC) {
-            System.out.println("Não forma triângulo");
+        double[] lado = new double[3];
+        for(int i = 0; i < 3; i++) {
+            lado[i] = input.nextDouble();
         }
+
+        Arrays.sort(lado);
+        double ladoA = lado[2];
+        double ladoB = lado[1];
+        double ladoC = lado[0];
 
         tipoTriangulo(ladoA, ladoB, ladoC);
 
