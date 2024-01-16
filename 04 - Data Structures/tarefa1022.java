8cae1f559b63bd19f57ee0cae3fdaf01d80a0c40
Gabriel Fernandes <gabriel.assuncao.fernandes07@aluno.ifce.edu.br>
2024-01-16 14:01:26 -0300

    04 - 1022

---
+++ 04 - Data Structures/tarefa1022.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class tarefa1022 {
+    public static void main(String[] args){
+
+        // Sum: (N1*D2 + N2*D1) / (D1*D2)
+        // Subtraction: (N1*D2 - N2*D1) / (D1*D2)
+        // Multiplication: (N1*N2) / (D1*D2)
+        // Division: (N1/D1) / (N2/D2), that means (N1*D2)/(N2*D1)
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite a primeira fração");
+
+        input.close();
+    }
+}
