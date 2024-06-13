20c57d241010c2e7941189ed0ced5f209455b11a
Gabriel Fernandes <gabrielassuncaofernandes@gmail.com>
2024-06-13 10:00:18 -0300

    1047

--- 01 - Beginner/tarefa1047.java
+++ 01 - Beginner/tarefa1047.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class tarefa1047 {
-    
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite horas e minutos inicial");
+
+        int hInicial = input.nextInt();
+        int mInicial = input.nextInt();
+
+        System.out.println("Digite horas e minutos final");
+        int hFinal = input.nextInt();
+        int mFinal = input.nextInt();
+
+        input.close();
+    }
 }
