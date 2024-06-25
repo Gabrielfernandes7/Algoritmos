c8551def647c1a4aaf31a33cfe25d8f983bd3435
Gabriel Fernandes <gabrielassuncaofernandes@gmail.com>
2024-06-25 15:57:19 -0300

    fazendo: 1049

---
+++ 01 - Beginner/tarefa1049.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class tarefa1049 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String palavra1 = sc.nextLine();
+        String palavra2 = sc.nextLine();
+        // String palavra3 = sc.nextLine();
+
+        if (palavra1 == "vertebrado") {
+            if (palavra2 == "carnívoro") {
+                
+            } else {
+
+            }
+        } else if (palavra1 == "invertebrado"){
+            
+        } else {
+            System.out.println("Entrada inválida");
+        }
+        
+        sc.close();
+    }
+}
